19b85cec35703ee720952b5d90566edcab4ae719
diegomarrufo <dmarrufo@ucm.es>
2022-03-02 22:28:35 +0100

    using my postdate component on post details

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-import moment from 'moment'
 import Image from 'next/image'
+import { PostDate } from '.'
 
 const PostDetail = ({ post }) => {
   return (
@@ -28,25 +28,7 @@ const PostDetail = ({ post }) => {
               {post.author.name}
             </p>
           </div>
-          <div className="font-medium text-gray-700">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="mr-2 inline h-6 w-6 text-violet-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-            <span className="align-middle">
-              {moment(post.createdAt).format('MMM DD, YYYY')}
-            </span>
-          </div>
+          <PostDate post={post} />
         </div>
         <div className="prose max-w-none">
           <ReactMarkdown>{post.content}</ReactMarkdown>
