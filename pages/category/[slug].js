910fd4e48975c0f17a9981505e5c7330c9578d9a
diegomarrufo <dmarrufo@ucm.es>
2022-03-04 15:46:12 +0100

    reversing the array resulted in error during build. Using now a different way to reverse the posts in category

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -5,8 +5,6 @@ import { getCategories, getCategoryPost } from '../../services'
 import { Layout, PostCard, Categories, Loader } from '../../components'
 
 const CategoryPost = ({ posts }) => {
-  const reversePosts = [...posts].reverse()
-
   const router = useRouter()
 
   if (router.isFallback) {
@@ -18,9 +16,12 @@ const CategoryPost = ({ posts }) => {
       <div className="container mx-auto mb-8 px-4 lg:px-10">
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-12">
           <div className="col-span-1 lg:col-span-8">
-            {reversePosts.map((post, index) => (
-              <PostCard key={index} post={post.node} />
-            ))}
+            {posts
+              .slice(0)
+              .reverse()
+              .map((post, index) => (
+                <PostCard key={index} post={post.node} />
+              ))}
           </div>
           <div className="col-span-1 lg:col-span-4">
             <div className="relative top-8 lg:sticky">
