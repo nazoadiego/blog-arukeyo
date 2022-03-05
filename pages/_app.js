4d55fa4ad2f17fe689531d3044890e33ba83ad22
diegomarrufo <dmarrufo@ucm.es>
2022-03-05 23:18:32 +0100

    fixed the head element not displaying on all pages. Added a cool favicon!

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import '../styles/globals.css'
 import '../styles/application.scss'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Arukeyo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/posts.js
+++ pages/posts.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { PostCard, Categories, PostWidget, Layout } from '../components'
 import { getPosts } from '../services'
 
@@ -9,11 +8,6 @@ const Home = ({ posts }) => {
   return (
     <Layout>
       <div className="container mx-auto mb-8 px-4 lg:px-10">
-        <Head>
-          <title>Arukeyo</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-12">
           <div className="col-span-1 lg:col-span-8">
             {reversePosts.map((post) => (
